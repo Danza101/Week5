b5d744bc6832dc39fa52cecb530425e7f0dd62cf
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-10-20 00:36:04 +0100

    Created a whole new week 5 Repo im acc doing in properlly now

---
+++ Test_Week_5.py
@@ -0,0 +1,18 @@
+from Week_5_Function import add_num;
+import unittest
+
+class TestFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add_num(3 , 2)
+        self.assertEqual(result, 5)
+        
+    def test_add_positive_and_negative_numbers(self):
+        result = add_num(3 , -2)
+        self.assertEqual(result, 1)
+        
+    def test_add_decimal_numbers(self):
+        result = add_num(3.2 , 2.2)
+        self.assertAlmostEqual(result, 5.4 , 0.3)
+        
+if __name__ == '__main__':
+    unittest.main()
