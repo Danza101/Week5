04ba38d28879e738ed098de28f6907c5dfbbeceb
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-10-20 10:48:58 +0100

    Error checking

--- Test_Week_5.py
+++ Test_Week_5.py
@@ -1,18 +1,20 @@
-from Week_5_Function import add_num;
+from Week_5_Function import add_num
 import unittest
+
 
 class TestFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
-        result = add_num(3 , 2)
+        result = add_num(3, 2)
         self.assertEqual(result, 5)
-        
+
     def test_add_positive_and_negative_numbers(self):
-        result = add_num(4 , -2)
+        result = add_num(4, -2)
         self.assertEqual(result, 2)
-        
+
     def test_add_decimal_numbers(self):
-        result = add_num(3.2 , 2.2)
-        self.assertAlmostEqual(result, 5.4 , 0.3)
-        
+        result = add_num(5.2, 2.2)
+        self.assertAlmostEqual(result, 5.4, 0.3)
+
+
 if __name__ == '__main__':
     unittest.main()
